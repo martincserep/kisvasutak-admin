6e319a71709592ad4718261a2d68abbf797c7dcd
Martin Cserép <martincserep1@gmail.com>
2020-02-17 19:49:43 +0100

    get one train

--- app.js
+++ app.js
@@ -50,6 +50,34 @@ app.get('/trains/:userId', function(request, response){
     })
 })
 
+app.get('/gettrain/:trainId', function(request, response){
+    response.header("Access-Control-Allow-Origin", "*" ); // update to match the domain you will make the request from
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    var sightsRef = database.ref("/trains")
+    var trainId = '-'+request.params.trainId
+    
+    sightsRef.once('value', function(snapshot){
+        var rawData = snapshot.val()
+        var data = []
+        var resp = []
+        Object.keys(rawData).map(item=> {
+            if(item==trainId){
+
+                data.push({'key': item, 'value': rawData[item]});
+            }
+        })
+        data.forEach(current => {
+            Object.keys(current).map(item=> {
+                resp.push(current[item])
+                     })
+                });
+        
+        response.send(resp)        
+        
+    })
+})
+
+
 app.get('/sights/:trainId', function(request, response){
     response.header("Access-Control-Allow-Origin", "*" ); // update to match the domain you will make the request from
     response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
@@ -104,9 +132,6 @@ app.get('/accomodations/:trainId', function(request, response){
     })
 })
 
-app.get('/csa/:name', function (req, res) {
-    console.log('Accessing the secret section ...')
-  res.send('csa ' + req.params.name)
-})
+
 
 app.listen(process.env.PORT || 8080, () => console.log('All is ok.'))
